971a2c67c62db05649db0be901fd7d1d5c35c3fe
李华林 <12131168@mail.sustech.edu.cn>
2024-08-07 17:19:20 +0800

    modify makefile

--- hyperbolic/run_make.sh
+++ hyperbolic/run_make.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-export GNUHOME=/data3/lihl/software/gcc-10.3.0-compile
-export NETCDF=/data3/lihl/software/disable-netcdf-4.4.1
+#export GNUHOME=/data3/lihl/software/gcc-10.3.0-compile
+#export NETCDF=/data3/lihl/software/disable-netcdf-4.4.1
+
+export GNUHOME=/usr
+export NETCDF=/data/apps/NetCDF/disable-netcdf-4.8.1
 
 echo
 echo "start to make ..."
